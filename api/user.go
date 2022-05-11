febaed95d923b5ed709d9781d9d990abe1d76e57
Jonathan Henrique de Moura <jonathanmourax4@gmail.com>
2022-05-11 11:17:50 -0300

    mock

--- api/user.go
+++ api/user.go
@@ -6,7 +6,15 @@ import (
 	"net/http"
 )
 
-func GetRandomUser(w http.ResponseWriter, r *http.Request) {
+type HTTPClientInterdace interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type Service struct {
+	HTTPClient HTTPClientInterdace
+}
+
+func (s *Service) GetRandomUser(w http.ResponseWriter, r *http.Request) {
 	apiURL := "https://randomuser.me/api"
 
 	req, err := http.NewRequest(http.MethodGet, apiURL, nil)
@@ -14,12 +22,17 @@ func GetRandomUser(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	res, err := http.DefaultClient.Do(req)
+	//res, err := http.DefaultClient.Do(req)
+	res, err := s.HTTPClient.Do(req)
 	if err != nil {
 		log.Println(err)
 	}
 	defer res.Body.Close()
+
 	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
--- api/user_test.go
+++ api/user_test.go
@@ -1,18 +1,41 @@
 package api
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
+
+func (mock *MockHTTP) Do(req *http.Request) (*http.Response, error) {
+	return mock.response, mock.err
+}
+
+type MockHTTP struct {
+	response *http.Response
+	err      error
+}
 
 func TestGetRandomUser(t *testing.T) {
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
 
 	w := httptest.NewRecorder()
 
-	GetRandomUser(w, r)
+	res := http.Response{
+		Body: ioutil.NopCloser(strings.NewReader("")),
+	}
+
+	mock := MockHTTP{
+		response: &res,
+	}
+
+	service := Service{
+		HTTPClient: &mock,
+	}
+
+	service.GetRandomUser(w, r)
 
 	got := w.Result()
 
